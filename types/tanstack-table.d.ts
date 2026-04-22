84552a890cac7df15be37b80fc06068e406f9ff3
bn4g2003 <nhatbangnguyenhet@gmail.com>
2026-04-22 17:35:12 +0700

    feat: update detail preview UI, add date_range filter type, and include sample Excel data files

--- types/tanstack-table.d.ts
+++ types/tanstack-table.d.ts
@@ -4,7 +4,7 @@ declare module "@tanstack/react-table" {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData, TValue> {
     filterKey?: string;
-    filterType?: "text" | "select" | "none";
+    filterType?: "text" | "select" | "date_range" | "none";
     filterOptions?: { value: string; label: string }[];
   }
 }
